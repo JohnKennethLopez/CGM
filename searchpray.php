be1e773fbfbfbd797ae7b375e9fa372bab007d78
JohnKennethLopez <lopeznneth204@gmail.com>
2022-11-22 20:18:41 +0800

    search design

--- searchpray.php
+++ searchpray.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CGM</title>
     <link rel="shortcut icon" type="image/png" href="css/image/icon.png">
-    <link rel="stylesheet" href="css/search.css">
+    <link rel="stylesheet" href="css/searchpray.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body>
